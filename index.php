7002e39343581c766eeca3ac2eee72f8414cafde
Claudio Coronel Milla <clau_coronelmilla@hotmail.com>
2020-08-04 01:28:26 -0300

    Inicializacion del proyecto
    Comienzo del proyecto marcos paz digital

---
+++ index.php
@@ -0,0 +1,257 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<!-- Head --> 
+<?php include("layout/head.php"); ?>
+
+<?php include("layout/nav.php"); ?>
+
+<body id="page-top">
+
+  <!-- About -->
+  <section class="content-section" id="about">
+    <div class="container">
+      <div class="row"> 
+          <div class="offset-md-3 col-md-6 p2 text-center">
+            <h1 class="alert-heading">Marcos Paz Digital</h1>
+            <h2>Un lugar, todas las aplicaciones</h2>
+            <br>
+          </div>
+        </div>
+        <div class="row text-center">
+          <div class="col-md-3 p1 hvr-float-shadow">
+            <a href="sistemas/sgas/">
+              <div class="alert alert-icon" style="background-color: #0097a7">
+                <i class="fa fa-ticket" style="color: #fff; font-size: 3rem; margin-bottom: 10px"></i>
+                <h4>Tickets</h4>
+                <hr>
+                <p>Sistema de Tickets para incidencias.</p>               
+              </div>
+            </a>
+          </div> 
+          <div class="col-md-3 p1 hvr-float-shadow">
+            <a href="sistemas/cuim/login.php">
+              <div class="alert alert-icon" style="background-color: #d32f2f">
+                <i class="fa fa-info-circle" style="color: #fff; font-size: 3rem; margin-bottom: 10px"></i>
+                <h4>CUIM</h4>
+                <hr>
+                <p >Sistema de Informacion Municipal.</p>               
+              </div>
+            </a>
+          </div>
+          <div class="col-md-3 p1 hvr-float-shadow">
+            <a href="sistemas/faltas">
+              <div class="alert alert-icon" style="background-color: #c2185b">
+                <i class="fa fa-car" style="color: #fff; font-size: 3rem; margin-bottom: 10px"></i>
+                <h4>Faltas</h4>
+                <hr>
+                <p >Multas de Transito y Comercio</p>               
+              </div>
+            </a>
+          </div>
+          <div class="col-md-3 p1 hvr-float-shadow">
+            <a href="sistemas/seguridad/login.php">
+              <div class="alert alert-icon" style="background-color: #7b1fa2">
+                <i class="fa fa-lock" style="color: #fff; font-size: 3rem; margin-bottom: 10px"></i>
+                <h4>Seguridad</h4>
+                <hr>
+                <p >COM Marcos Paz</p>              
+              </div>
+            </a> 
+          </div>
+          <div class="col-md-3 p1 hvr-float-shadow">
+            <a href="sistemas/suministros/login.php">
+              <div class="alert alert-icon" style="background-color: #00897b">
+                <i class="fa fa-briefcase" style="color: #fff; font-size: 3rem; margin-bottom: 10px"></i>
+                <h4>Suministros</h4>
+                <hr>
+                <p >Sistema interno de Suministros</p>               
+              </div>
+            </a>
+          </div>
+          <div class="col-md-3 p1 hvr-float-shadow">
+            <a href="sistemas/suministros/login.php">
+              <div class="alert alert-icon" style="background-color: #689f38">
+                <i class="fa fa-sun-o" style="color: #fff; font-size: 3rem; margin-bottom: 10px"></i>
+                <h4>Clima</h4>
+                <hr>
+                <p >Datos Online de las Estaciones Meteorológicas de Marcos Paz</p>              
+              </div>
+            </a> 
+          </div>
+          <div class="col-md-3 p1 hvr-float-shadow">
+            <a href="permisocirculacion/">
+              <div class="alert alert-icon" style="background-color: #9575cd">
+                <i class="fa fa-credit-card-alt" style="color: #fff; font-size: 3rem; margin-bottom: 10px"></i>
+                <h4>Permiso de Circulación Local</h4>
+                <hr>
+                <p >Formulario para Obtener permiso de Circulación</p>               
+              </div>
+            </a>
+          </div>
+          <div class="col-md-3 p1 hvr-float-shadow">
+            <a href="cuidarte/">
+              <div class="alert alert-icon" style="background-color: #29b6f6">
+                <i class="fa fa-medkit" style="color: #fff; font-size: 3rem; margin-bottom: 10px"></i>
+                <h4>Cuidarte</h4>
+                <hr>
+                <p >Aplicacion Cuidar para Empleados Municipales</p>               
+              </div>
+            </a>
+          </div>
+          <div class="col-md-3 p1 hvr-float-shadow">
+            <a href="registroempleadosmunicipales/">
+              <div class="alert alert-icon" style="background-color: #006064">
+                <i class="fa fa-users" style="color: #fff; font-size: 3rem; margin-bottom: 10px"></i>
+                <h4>Registro Municipal</h4>
+                <hr>
+                <p >Aplicación de Registro de Empleados Municipales</p>               
+              </div>
+            </a>
+          </div>
+          <div class="col-md-3 p1 hvr-float-shadow">
+            <a href="buzondesugerencias/">
+              <div class="alert alert-icon" style="background-color: #f57f17">
+                <i class="fa fa-inbox" style="color: #fff; font-size: 3rem; margin-bottom: 10px"></i>
+                <h4>Buzón de Sugerencias</h4>
+                <hr>
+                <p >Hacé tu sugerencia, la cual nos ayuda a mejorar</p>               
+              </div>
+            </a>
+          </div>
+          <div class="col-md-3 p1 hvr-float-shadow">
+            <a href="comproenmipueblo/">
+              <div class="alert alert-icon" style="background-color: #827717">
+                <i class="fa fa-cart-plus" style="color: #fff; font-size: 3rem; margin-bottom: 10px"></i>
+                <h4>Compro en Mi Pueblo</h4>
+                <hr>
+                <p >Una forma diferente de comprar</p>               
+              </div>
+            </a>
+          </div>
+          <div class="col-md-3 p1 hvr-float-shadow">
+            <a href="reactivar/">
+              <div class="alert alert-icon" style="background-color: #1de9b6">
+                <i class="fa fa-line-chart" style="color: #fff; font-size: 3rem; margin-bottom: 10px"></i>
+                <h4>Reactivar</h4>
+                <hr>
+                <p >Banco de Maquinarias, Herramientas y Materiales</p>               
+              </div>
+            </a>
+          </div>
+          <div class="col-md-3 p1 hvr-float-shadow">
+            <a href="http://webmail.marcospaz.net/">
+              <div class="alert alert-icon" style="background-color: #82c024">
+                <i class="fa fa-envelope" style="color: #fff; font-size: 3rem; margin-bottom: 10px"></i>
+                <h4>WebMail</h4>
+                <hr>
+                <p >Acceso directo</p>               
+              </div>
+            </a>
+          </div>
+          <div class="col-md-3 p1 hvr-float-shadow">
+            <a href="https://www.marcospaz.gov.ar/">
+              <div class="alert alert-icon" style="background-color: #1b5e20">
+                <i class="fa fa-globe" style="color: #fff; font-size: 3rem; margin-bottom: 10px"></i>
+                <h4>Municipalidad de Marcos Paz</h4>
+                <hr>
+                <p >Portal de Noticias</p>               
+              </div>
+            </a>
+          </div>
+          <div class="col-md-3 p1 hvr-float-shadow">
+            <a href="https://www.facebook.com/modernizacionmarcospaz">
+              <div class="alert alert-icon" style="background-color: #303f9f">
+                <i class="fa fa-facebook-square" style="color: #fff; font-size: 3rem; margin-bottom: 10px"></i>
+                <h4>Modernización</h4>
+                <hr>
+                <p >Página de Facebook</p>               
+              </div>
+            </a>
+          </div>
+          <div class="col-md-3 p1 hvr-float-shadow">
+            <a href="https://www.instagram.com/modernizacionmmp/">
+              <div class="alert alert-icon" style="background-color: #e91e63">
+                <i class="fa fa-instagram" style="color: #fff; font-size: 3rem; margin-bottom: 10px"></i>
+                <h4>Modernización</h4>
+                <hr>
+                <p >Página de Instagram</p>               
+              </div>
+            </a>
+          </div>                                                                                                 
+          <div class="col-md-3 p1 hvr-float-shadow">
+            <a href="turnos/">
+              <div class="alert alert-icon" style="background-color: #e67e22">
+                <i class="fa fa-building" style="color: #fff; font-size: 3rem; margin-bottom: 10px"></i>
+                <h4>Turnos</h4>
+                <hr>
+                <p >Sistema de Turnos</p>               
+              </div>
+            </a>
+          </div>
+          <div class="col-md-3 p1 hvr-float-shadow">
+            <a href="autotest">
+              <div class="alert alert-icon" style="background-color: #c0392b">
+                <i class="fa fa-stethoscope" style="color: #fff; font-size: 3rem; margin-bottom: 10px"></i>
+                <h4>Autotest COVID-19</h4>
+                <hr>
+                <p >Aplicación para realizar un autotest</p>               
+              </div>
+            </a>
+          </div>
+          <div class="col-md-3 p1 hvr-float-shadow">
+            <a href="preciosmaximos/">
+              <div class="alert alert-icon" style="background-color: #2c3e50">
+                <i class="fa fa-usd" style="color: #fff; font-size: 3rem; margin-bottom: 10px"></i>
+                <h4>Precios Máximos</h4>
+                <hr>
+                <p >Listado de Precios de Referencia</p>               
+              </div>
+            </a>
+          </div>
+          <div class="col-md-3 p1 hvr-float-shadow">
+            <a href="protocolocomercios/">
+              <div class="alert alert-icon" style="background-color: #718093">
+                <i class="fa fa-thumbs-up" style="color: #fff; font-size: 3rem; margin-bottom: 10px"></i>
+                <h4>Protocolo Comercios COVID-19</h4>
+                <hr>
+                <p >Presentacion de Protocolo para Comercios</p>               
+              </div>
+            </a>
+          </div>
+          <div class="col-md-3 p1 hvr-float-shadow">
+            <a href="formulariocomercios/">
+              <div class="alert alert-icon" style="background-color: #40739e">
+                <i class="fa fa-wpforms" style="color: #fff; font-size: 3rem; margin-bottom: 10px"></i>
+                <h4>Formulario Comercios </h4>
+                <hr>
+                <p >Formularios para Comercios</p>               
+              </div>
+            </a>
+          </div>
+          <div class="col-md-3 p1 hvr-float-shadow">
+            <a href="expedientes/">
+              <div class="alert alert-icon" style="background-color: #ea8685">
+                <i class="fa fa-address-book-o" style="color: #fff; font-size: 3rem; margin-bottom: 10px"></i>
+                <h4>Expedientes</h4>
+                <hr>
+                <p >Aplicación de Expedientes Digitales</p>               
+              </div>
+            </a>
+          </div>
+          <br>                   
+        </div>          
+        </div>          
+        </div>
+      </div>
+    </div>
+  </section>
+
+
+  <?php include("layout/footer.php"); ?>
+
+  <?php include("layout/script.php"); ?>
+
+</body>
+
+</html>
